2b568161afab687a41a526f0f6c963293162fbf2
David Lopez Rodriguez <dloprodu@gmail.com>
2018-09-25 23:41:22 +0100

    image picker added

--- src/components/sections/characterAdd/view.js
+++ src/components/sections/characterAdd/view.js
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { Button, TextInput } from '../../widgets/';
+
+import ImagePicker from 'react-native-image-picker';
 
 import styles from './styles';
 
 export default class extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            age: '',
+            image: ''
+        };
+
+        this.options = {
+            title: 'Select image',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images'
+            }
+        };
+    }
+
+    _onImagePickerTapped() {
+        console.log(this.options);
+        console.log(ImagePicker);
+
+        if (!ImagePicker) {
+            return
+        }
+
+        ImagePicker.showImagePicker(this.options, (response) => {
+            console.log('Response = ', response);
+        
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                let source = { uri: response.uri };
+            
+                // You can also display the image using data:
+                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+            
+                this.setState({
+                    image: source
+                });
+            }
+        });
+    }
+
+    _renderImageInput() {
+        const imageUri = this.state.avatarSource ? { uri: this.state.avatarSource.uri } : null
+        return (
+            <View>
+                <TouchableOpacity style={{}} onPress={() => this._onImagePickerTapped()}>
+                    <Image source={imageUri} style={{ width: 200, height: 200 }} resizeMode={'contain'} />
+                    <Text style={{color: 'white', fontWeight: 'bold'}}>{'Pulsa para elegir imagen'}</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
+                <View style={{padding: 20}}>
+                    <TextInput 
+                        label={'Nombre del personaje:'}
+                        value={this.state.name}
+                        onChangeText={ name => this.setState({ name }) }
+                        placeholder={'Eddard Stark'}
+                        />
+                </View>
+                <View style={{padding: 20}}>
+                    <TextInput 
+                        label={'Edad del personaje:'}
+                        value={this.state.age}
+                        onChangeText={ age => this.setState({ age }) }
+                        placeholder={'32'}
+                        />
+                </View>
 
+                <View style={{ paddingHorizontal: 20, paddingBottom: 40}}>
+                    { this._renderImageInput() }
+                </View>
+
+                <View style={{paddingHorizontal: 20, paddingBottom: 20}}>
+                    <Button label={'Guardar'.toUpperCase()} />
+                </View>
             </View>
         )
     }
